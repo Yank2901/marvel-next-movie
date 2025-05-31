00238da6f6d5cbd7183d2b0e2f06ad2b7118f9a9
Yanick <yanick290101@gmail.com>
2025-05-31 01:59:58 -0500

    feat: crear una sesion CURL para el consumo de API con informacion para la pagina

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
+const API_URL = 'https://whenisthenextmcufilm.com/api';
+// Inicializar una nueva sesión de CURL; ch = CURL handle
+$ch = curl_init(API_URL);
+// Indicar que queremos recibir la respuesta de la petición y no que se imprima directamente
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Ejecutar la petición y almacenar la respuesta
+$response = curl_exec($ch);
+$data = json_decode($response, true);
+// Cerrar la sesión de CURL
+curl_close($ch);
+
+var_dump($data);
+
 ?>
 
 <h1>La proxima película de Marvel</h1>
