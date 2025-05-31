38d73ecdcc2b8d69ae340ca1501d22474575799a
Yanick <yanick290101@gmail.com>
2025-05-31 02:27:45 -0500

    feat: mostrar informacion de la pelicula del MCU proxima a estrenarse con su imagen y un diseño base

--- index.php
+++ index.php
@@ -11,11 +11,32 @@ $data = json_decode($response, true);
 // Cerrar la sesión de CURL
 curl_close($ch);
 
-var_dump($data);
-
 ?>
 
-<h1>La proxima película de Marvel</h1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta description="Información sobre la próxima película de Marvel">
+    <title>Próxima película de Marvel</title>
+    <!-- Centered viewport -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css">
+</head>
+
+<main>
+    <section class="tilt-card">
+        <img 
+            src = <?= $data['poster_url'] ?>
+            alt = "Poster de <?= $data['title'] ?>"
+            width = "300"
+            loading = "lazy"
+        />
+    </section>
+    <hgroup>
+        <h2><?= $data['title'] ?> se estrena en <?= $data["days_until"] ?> días</h2>
+        <p>Fecha de estreno: <?= $data["release_date"] ?></p>
+        <p> La siguiente es: <?= $data["following_production"]["title"] ?> </p>
+    </hgroup>
+</main>
 
 <style>
     :root {
@@ -23,7 +44,22 @@ var_dump($data);
     }
 
     body {
-        display: grid;
-        place-content: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+    }
+
+    main {
+        text-align: center;
+        max-width: 600px;
+        padding: 20px;
+    }
+
+    img {
+        display: block;
+        margin: 0 auto 20px;
     }
 </style>
