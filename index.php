138d695140d02be9256c84cc1085d2c367d2ebc5
Yanick <yanick290101@gmail.com>
2025-05-31 01:36:17 -0500

    feat: crear index.php con estructura básica

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<h1>La proxima película de Marvel</h1>
+
+<style>
+    :root {
+        color-scheme: dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+</style>
